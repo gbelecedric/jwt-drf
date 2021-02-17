499af049c125d0b0e8d1618ee91aa05956bb97a6
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-17 00:19:01 +0000

    .

--- apiwithjwt/core/views.py
+++ apiwithjwt/core/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated  # <-- Here
+
 
 class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)             # <-- And here
+
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
