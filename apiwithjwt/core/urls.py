4abac191a9ed8208d6587c169c3161dc1d53019b
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-17 00:44:04 +0000

    Update urls.py

--- apiwithjwt/core/urls.py
+++ apiwithjwt/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from core.views import *
+from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
+from myapi.core import views
 
 urlpatterns = [
-    path('hello/', HelloView.as_view(), name='hello'),
+    path('hello/', views.HelloView.as_view(), name='hello'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # <-- And here
 ]
