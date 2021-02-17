b8bc05b284ee6e46ee4f508ca56be0daf5aa668f
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-17 00:21:43 +0000

    .

--- apiwithjwt/apiwithjwt/settings.py
+++ apiwithjwt/apiwithjwt/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-Party Apps
     'rest_framework',
+    'rest_framework.authtoken',  # <-- Here
 
     'core',
 ]
