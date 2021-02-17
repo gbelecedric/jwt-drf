0a813791bccb2e1be9824594d77920f17fcaf0e5
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-17 00:07:01 +0000

    r

--- apiwithjwt/core/views.py
+++ apiwithjwt/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloView(APIView):
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
