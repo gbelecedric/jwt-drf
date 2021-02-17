aaa874cd02291aa4536ac558f883b6edaf0428b8
gbelecedric <cedric.gbele@uvci.edu.ci>
2021-02-17 00:46:01 +0000

    Update urls.py

--- apiwithjwt/core/urls.py
+++ apiwithjwt/core/urls.py
@@ -3,6 +3,6 @@ from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
 from core.views import *
 
 urlpatterns = [
-    path('hello/', views.HelloView.as_view(), name='hello'),
+    path('hello/', HelloView.as_view(), name='hello'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # <-- And here
 ]
